8e11678dcbe5d4cdb2d0d1dd61560810ca525f6e
himansh <hssharma2212000@gmail.com>
2019-11-21 03:04:32 +0530

    addtodo

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { throwStatement } from '@babel/types';
 class AutoTodo extends Component {
     state = { title:"" }
     onChange=(e)=>{
         this.setState({title:e.target.value});
     }
+    onSubmit=(e)=>{
+        e.preventDefault();
+        throwStatement.props.todo(this.state.title);
+        this.setState({title : " "})
+    }
     render() { 
         return (
             
-            <form style={{display:'flex',height:'70px'}}>
+            <form onSubmit={this.onSubmit} style={{display:'flex',height:'70px'}}>
 
             <input type="text" name="title" placeholder="add todo" style={{flex:'10',width:'90%',padding:'5px'}} value={this.state.title} onChange={this.onChange}></input>
             <input type="submit" value="submit" classNmae="btn" style={{flex:'1'}}></input>
