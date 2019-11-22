b0f896cb66d813e94b963c4dc391af4a8ac68dde
himansh <hssharma2212000@gmail.com>
2019-11-23 00:59:50 +0530

    Updates

---
+++ precache-manifest.bbbb026b9143f9f9fcd6cc071da19808.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7e9aee8ab90bb0cefeb72645409349",
+    "url": "/REACT-APP-TODO/index.html"
+  },
+  {
+    "revision": "908e84a36446146da01b",
+    "url": "/REACT-APP-TODO/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "8aeebe3526e2ee090b70",
+    "url": "/REACT-APP-TODO/static/js/2.77272e0a.chunk.js"
+  },
+  {
+    "revision": "908e84a36446146da01b",
+    "url": "/REACT-APP-TODO/static/js/main.52a8cb35.chunk.js"
+  },
+  {
+    "revision": "d345d0709d0de556c973",
+    "url": "/REACT-APP-TODO/static/js/runtime-main.670c9c19.js"
+  }
+]);
